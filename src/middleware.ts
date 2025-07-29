fbf247d9156598d370fccd4b39e10487f26665d7
Aileen Tandoc <tandocaileen@gmail.com>
2025-07-29 04:52:17 +0000

    change / to home. no page should be displayed in /
    FIX THE LOGIN

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,15 +6,28 @@ export function middleware(request: NextRequest) {
   const { pathname } = request.nextUrl
   const isLoggedIn = request.cookies.has('ltoportal-user')
 
-  // If the user is logged in, and tries to access /login, redirect to home
-  if (isLoggedIn && pathname === '/login') {
-    return NextResponse.redirect(new URL('/home', request.url))
+  const isApiRoute = pathname.startsWith('/api');
+  const isStaticFile = pathname.startsWith('/_next/static') || pathname.startsWith('/_next/image') || pathname === '/favicon.ico';
+
+  if (isApiRoute || isStaticFile) {
+    return NextResponse.next();
   }
 
-  // If the user is not logged in and is trying to access any page other than /login,
-  // redirect them to /login.
-  if (!isLoggedIn && pathname !== '/login') {
-    return NextResponse.redirect(new URL('/login', request.url))
+  // If the user is logged in
+  if (isLoggedIn) {
+    // and tries to access /login, redirect to home
+    if (pathname === '/login') {
+      return NextResponse.redirect(new URL('/home', request.url))
+    }
+    // and tries to access /, redirect to home
+    if (pathname === '/') {
+        return NextResponse.redirect(new URL('/home', request.url))
+    }
+  } else { // If the user is not logged in
+    // and is trying to access any page other than /login, redirect them to /login.
+    if (pathname !== '/login') {
+      return NextResponse.redirect(new URL('/login', request.url))
+    }
   }
 
   return NextResponse.next()
@@ -23,13 +36,6 @@ export function middleware(request: NextRequest) {
 // See "Matching Paths" below to learn more
 export const config = {
   matcher: [
-    /*
-     * Match all request paths except for the ones starting with:
-     * - api (API routes)
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     */
     '/((?!api|_next/static|_next/image|favicon.ico).*)',
   ],
 }
